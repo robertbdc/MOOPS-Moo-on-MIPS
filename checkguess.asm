1faba194cb930fbaaebf8d59a9ce4679a200d304
Robert Brooks <rab120130@utdallas.edu>
2013-10-12 19:17:13 -0500

    Add Bulls check

--- checkguess.asm
+++ checkguess.asm
@@ -4,30 +4,31 @@
 #.globl checkguess
 # Params:	$a0 = guess
 #		$a1 = correct answer
-# Returns:	$v0 = byte containing status, see below
-# Temps:	$t0 = copy of guess
+# Returns:	$v0 = status, see below
+
+# Lower 16 bits of $v0 will be set as follows:
+# i000 g000 0bbb 0ccc
+#
+# i = 1 for invalid (0x8000)
+# g = 1 if already guessed (0x0800) - I may still need this for the AI
+# bbb = 0-4 bulls (0x0040 means win)
+# ccc = 0-4 cows (0x0004 means all present but in wrong order)
+
+# Some temps used here (for reference when debugging):
+#		$t0 = copy of guess
 #		$t1 = result after XOR
 #		$t2 = digit we're checking
+#		$t3 = bull/cow count
 #		$t7 = counter
 
-# Each byte in the return has these bitwise flags:
-# igbbbccc
-#
-# i = bit set if invalid (allows us to compare with 0 (signed)
-#     or 0x80 (unsigned) to determine "invalid")
-# g = bit set if user guessed this one already (compare with 0x40)
-# bbb = bulls (value 0-4, 3 bits, 000-100) (compare with 0x20 to determine "you won")
-# ccc = cows (value 0-4, 3 bits, 000-100)
-
 checkguess:
-	# Simplest case: is the guess right?
-	beq	$a0, $a1, correct	# breakpoint: first statement in checkguess
+	beq	$a0, $a1, correct	# Simplest case: is the guess right?
 
 	# Is the guess valid?
 	add	$t0, $a0, $zero		# make a copy of guess
 	add	$t7, $zero, $zero	# init counter
 rollme:
-	# End result of this bit: rotate one hex digit, ABCD becomes BCDA
+	# End result of this section: rotate one hex digit, ABCD becomes BCDA
 	# (like a 'rol' on just the bottom half)
 	sll	$t6, $t0, 4		# last 4 of $t6 are BCD0
 	srl	$t5, $t0, 12		# last 4 of $t5 are 000A
@@ -47,20 +48,42 @@ rollme:
 	# made it, try next digit
 	addi	$t7, $t7, 1
 	blt	$t7, 3, rollme		# Repeat 3x (after 4, it is back like it started!)
+
+	# Guess is valid. Let's see how well it did.
+	add	$v0, $zero, $zero	# initialize return value
+		
+	# Check for bulls
+	add	$t3, $zero, $zero	# initialize bull count
+	xor 	$t1, $a0, $a1		# If a bit is the same, result is 0
+	andi 	$t2, $t1, 0xF000	# Check digit
+	bne	$t2, $zero, chk2
+	addi	$t3, $t3, 1		# Found one!
+chk2:	andi 	$t2, $t1, 0x0F00	# Check digit
+	bne	$t2, $zero, chk3
+	addi	$t3, $t3, 1		# Found one!
+chk3:	andi 	$t2, $t1, 0x00F0	# Check digit
+	bne	$t2, $zero, chk4
+	addi	$t3, $t3, 1		# Found one!
+chk4:	andi 	$t2, $t1, 0x000F	# Check digit
+	bne	$t2, $zero, setbull
+	addi	$t3, $t3, 1		# Found one!
+
+setbull:
+	sll	$t3, $t3, 4		# Bulls go here: 00x0
+	or	$v0, $v0, $t3
 	
-	# todo: check bulls and cows
-	
+	# Check for cows
+		
 # return labels
-nothing:
-	add	$v0, $zero, $zero	# You got nothin!
+counted:
 	jr	$ra
 
 correct:
-	add	$v0, $zero, 0x24	# The magic number! 00 100 100 = 4 bulls, 4 cows
+	add	$v0, $zero, 0x0040	# The magic number! 4 bulls 0 cows
 	jr	$ra
 			
 badpat:
-	add	$v0, $zero, 0x80	# 1xxx xxxx = bad pattern
+	add	$v0, $zero, 0x8000	# bad pattern
 	jr	$ra
 	
 	

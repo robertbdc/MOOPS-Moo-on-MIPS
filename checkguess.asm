a6ac08e60c2ec2fb0bac472690ee01026db453e8
Robert Brooks <rab120130@utdallas.edu>
2013-10-12 20:16:33 -0500

    Now counts and returns bulls and cows!

--- checkguess.asm
+++ checkguess.asm
@@ -18,8 +18,11 @@
 #		$t0 = copy of guess
 #		$t1 = result after XOR
 #		$t2 = digit we're checking
-#		$t3 = bull/cow count
+#		$t3 = match count
+#		$t4 = bulls
+#		$t5 = cows
 #		$t7 = counter
+#		$t8, $t9 = scratch vars
 
 checkguess:
 	beq	$a0, $a1, correct	# Simplest case: is the guess right?
@@ -30,9 +33,9 @@ checkguess:
 rollme:
 	# End result of this section: rotate one hex digit, ABCD becomes BCDA
 	# (like a 'rol' on just the bottom half)
-	sll	$t6, $t0, 4		# last 4 of $t6 are BCD0
-	srl	$t5, $t0, 12		# last 4 of $t5 are 000A
-	or	$t0, $t5, $t6		# last 4 of $t0 are BCDA
+	sll	$t8, $t0, 4		# last 4 of $t8 are BCD0
+	srl	$t9, $t0, 12		# last 4 of $t9 are 000A
+	or	$t0, $t9, $t8		# last 4 of $t0 are BCDA
 	andi	$t0, $t0, 0xFFFF	# dump the A that got shifted into first 4
 
 	# Now check each digit for sameness
@@ -49,12 +52,25 @@ rollme:
 	addi	$t7, $t7, 1
 	blt	$t7, 3, rollme		# Repeat 3x (after 4, it is back like it started!)
 
+
 	# Guess is valid. Let's see how well it did.
-	add	$v0, $zero, $zero	# initialize return value
+	# Check for bulls (first pass) or cows (other passes)
+	add	$t4, $zero, $zero	# initialize bulls
+	add	$t5, $zero, $zero	# initialize cows
 		
-	# Check for bulls
-	add	$t3, $zero, $zero	# initialize bull count
-	xor 	$t1, $a0, $a1		# If a bit is the same, result is 0
+	add	$t0, $a0, $zero		# make a copy of guess
+	add	$t7, $zero, $zero	# init counter
+	# Different from the bad value loop because the first check doesn't rotate
+	j	checkbullcow
+rollbullcow:
+	# (Rotate is just like the bad value loop)
+	sll	$t8, $t0, 4		# last 4 of $t8 are BCD0
+	srl	$t9, $t0, 12		# last 4 of $t9 are 000A
+	or	$t0, $t9, $t8		# last 4 of $t0 are BCDA
+	andi	$t0, $t0, 0xFFFF	# dump the A that got shifted into first 4
+checkbullcow:	
+	add	$t3, $zero, $zero	# initialize match count
+	xor 	$t1, $t0, $a1		# If a bit is the same, result is 0
 	andi 	$t2, $t1, 0xF000	# Check digit
 	bne	$t2, $zero, chk2
 	addi	$t3, $t3, 1		# Found one!
@@ -65,14 +81,26 @@ chk3:	andi 	$t2, $t1, 0x00F0	# Check digit
 	bne	$t2, $zero, chk4
 	addi	$t3, $t3, 1		# Found one!
 chk4:	andi 	$t2, $t1, 0x000F	# Check digit
-	bne	$t2, $zero, setbull
+	bne	$t2, $zero, setmatch
 	addi	$t3, $t3, 1		# Found one!
 
-setbull:
-	sll	$t3, $t3, 4		# Bulls go here: 00x0
-	or	$v0, $v0, $t3
+setmatch:
+	bne	$t7, $zero, setcow
+	# First time through is bulls
+	add	$t4, $t4, $t3
+	j	nextdigit
+setcow:
+	# Add to cow count
+	add	$t5, $t5, $t3
 	
-	# Check for cows
+	# made it, try next digit
+nextdigit:
+	addi	$t7, $t7, 1
+	blt	$t7, 4, rollbullcow		# Repeat 4x
+	
+	# set bulls and cows in return value
+	sll	$v0, $t4, 4		# Bulls go here: 00x0
+	or	$v0, $v0, $t5		# Cows go here: 000x
 		
 # return labels
 counted:

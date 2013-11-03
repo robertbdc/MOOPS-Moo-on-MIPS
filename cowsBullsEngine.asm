030a30978cdc917313cabe2343c1c6a65e8ba611
Andrew Paettie <ajp073000@utdallas.edu>
2013-11-03 13:41:21 -0600

    store guesses (and result of guesses) now

--- cowsBullsEngine.asm
+++ cowsBullsEngine.asm
@@ -40,7 +40,19 @@ numberOfBullsString:
 numberOfCowsString:
 		.asciiz "Number of Cows: "
 		.align 2
-
+#store preious guesses as 4 bytes of ASCII		
+playerPreviousGuess:
+#how much space do we need? how many possible guesses are there?
+#16 choose 4 = 16*15*14*13 = 43680 guesses * 4bytes/guess = 174720 bytes
+		.space 174720
+		.align 2		
+playerPreviousResults:
+#really only need 3 bits for each guess, because the most cows/bulls you can have is 4
+#lets align this one on the half word to make life easier (who wants a 6 bit array?)
+#least signifigant byte of 1/2 word will hold number of cows
+#most signifigant byte of halfword will hold number of bulls
+		.space 87360 #174720/2
+		.align 1
 .text
 
 .globl main
@@ -53,7 +65,7 @@ numberOfCowsString:
 main:
 	la $a0, humanPromptText
 	jal printText
-	j generateComputerSecretNumber
+	#j generateComputerSecretNumber
 	
 inputSecretNumber:
 	li $a1, 5
@@ -90,6 +102,9 @@ checkLoop:
 	la $a0, computerPromptText #this is all for debugging purposes
 	jal printText
 	move $a0, $s0
+	jal printText #print the comp's secret number
+	jal printNewline
+	move $a0, $s0
 	jal atoi
 	sw $v0, computerSecretNumber
 setupBasics:
@@ -106,10 +121,13 @@ humanTurnCallback:
 	la $a0, playerInputBuffer	#the input buffer for the
 	li $a1, 5			#max number of characters
 	jal readString
+	lw $a1, ($a0)
+	storeArrayWord(playerPreviousGuess, turnNumber, $a1) #save the guess in the array of guesses
 	jal atoi
 	move $a0, $v0
 	lw $a1, computerSecretNumber
 	jal checkguess
+	storeArrayHalfWord(playerPreviousResults, turnNumber, $v0)	#save the result in the array of results
 	move $s0, $v0
 	jal printNewline
 	move $a0, $s0
@@ -128,7 +146,7 @@ errorOut:
 	
 compareGuess:
 	move $t1, $a0 
-	move $ra, $t7	#$t7 is the return address
+	move $t7, $ra	#$t7 is the return address
 	andi $t0, $t1, 0xF000	#check for validity
 	beq $t0, 8 errorOut
 	andi $t0, $t1, 0x00F0
@@ -137,10 +155,16 @@ compareGuess:
 	jal printText
 	move $a0, $t0
 	jal printInteger
+	push($a0)
+	jal printNewline
+	pop($a0)
 	andi $t0, $t1, 0x000F
 	la $a0, numberOfCowsString
 	jal printText
 	move $a0, $t0
 	jal printInteger
+	push($a0)
+	jal printNewline
+	pop($a0)
 	jr $t7	#I stored off the return address in $t7
 	
